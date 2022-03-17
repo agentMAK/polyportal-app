4dab5f5f579995665537581df1a0ab1f108ce7c9
agentMAK <martin0044@gmail.com>
2022-03-17 19:21:38 +0000

    Photo Typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import doorway from '../public/images/doorway.png'
 import Image from 'next/image'
 import uniswap from '../public/images/uniswap-grants.png'
-import polygon from '../public/images/Polygon-logo.svg'
+import polygon from '../public/images/polygon-logo.svg'
 import NavBar from '../components/elements/NavBar'
 import Meta from '../components/meta'
 
