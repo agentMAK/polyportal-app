8b3d3b027838f12aa4f36c5a3dafd12306224d15
agentMAK <martin0044@gmail.com>
2022-04-14 09:11:15 +0100

    directory fix 2

--- components/learn/web3/setupMetamask/L0_SetupMetamask.tsx
+++ components/learn/web3/setupMetamask/L0_SetupMetamask.tsx
@@ -1,11 +1,11 @@
 import AppHeader from "../../../elements/App/AppHeader"
 import SplitView from "../../../elements/App/SplitView"
-import metamaskLogo from '../../../../public/images/metamaskFox.png'
+import web3 from '../../../../public/images/learn/web3/what-is-web3/web3future.jpeg'
 
 const L0_SetupMetamask = (props:any) => {
     return (<main className='w-full h-full flex flex-col'>
         <AppHeader title="Installing Metamask">MetaMask is a popular browser-based wallet that makes it easy to use your Ethereum account. We are going install MetaMask to create and wallet and get you all set up. </AppHeader>
-        <SplitView image={metamaskLogo}>
+        <SplitView image={web3}>
               <p className='text-xl'>You can download the MetaMask browser extention at: </ p>
             <br />
             <a href="https://metamask.io/download/" target="_blank" rel="noopener noreferrer" className='text-xl font-sans-semibold text-primary hover:underline'>https://metamask.io/download</a></SplitView>
