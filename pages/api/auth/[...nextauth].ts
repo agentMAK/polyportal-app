34caad4df0cf3bd0ee95013ae9a4ded3a3e96778
agentMAK <martin0044@gmail.com>
2022-04-27 09:35:45 +0100

    Mixpanel Added

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,27 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
 import DiscordProvider from "next-auth/providers/discord";
+var Mixpanel = require('mixpanel');
+
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     DiscordProvider({
         clientId: process.env.DISCORD_CLIENT_ID,
-        clientSecret: process.env.DISCORD_CLIENT_SECRET
+        clientSecret: process.env.DISCORD_CLIENT_SECRET,
+        authorization: { params: { scope: 'identify guilds' } },
       })
   ],
+  events: {
+    async signIn({ user, account, profile}) {
+      var mixpanel = Mixpanel.init(`${process.env.NEXT_PUBLIC_MIXPANEL_PROJECT_ID}`, {
+        debug: true,
+        api_host: "https://api-eu.mixpanel.com",
+      });
+
+      mixpanel.people.set(user.id, {$first_name: user.name});
+      mixpanel.track('Sign In', { distinct_id: user.id,});
+    
+    },
+  }
 })
