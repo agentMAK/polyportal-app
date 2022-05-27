199d0d6f0ce1b069258fcda19703459891aee85c
agentMAK <martin0044@gmail.com>
2022-05-27 14:31:15 +0100

    Confetti Added

--- components/app/lesson/DisplaySlide.tsx
+++ components/app/lesson/DisplaySlide.tsx
@@ -1,9 +1,50 @@
 import styled from "@emotion/styled";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../../main/Button";
+import confetti from "canvas-confetti";
 
 // eslint-disable-next-line react/display-name
 const Slide = React.forwardRef((props: any, ref: any) => {
+  const [confettiToggle, setConfettiToggle] = useState<boolean>(false);
+
+  const showConfetti = () => {
+    var count = 200;
+    var defaults = {
+      origin: { y: 0.9 },
+    };
+
+    function fire(particleRatio: any, opts: any) {
+      confetti(
+        Object.assign({}, defaults, opts, {
+          particleCount: Math.floor(count * particleRatio),
+        })
+      );
+    }
+
+    fire(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+    fire(0.2, {
+      spread: 60,
+    });
+    fire(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
+  };
+
   let button: any = (
     <div className="w-full flex justify-end">
       {" "}
@@ -20,15 +61,21 @@ const Slide = React.forwardRef((props: any, ref: any) => {
   );
 
   if (props.isStart) {
-    return <SlideIntro ref={ref}>
-            {props.children}
-            {props.displayButton ? button : null}
-            <br />
-            <br />
-          </SlideIntro>
+    return (
+      <SlideIntro ref={ref}>
+        {props.children}
+        {props.displayButton ? button : null}
+        <br />
+        <br />
+      </SlideIntro>
+    );
   }
 
   if (props.isEnd) {
+    if (!confettiToggle) {
+      showConfetti();
+      setConfettiToggle(true);
+    }
     button = (
       <div className="w-full flex justify-center">
         {" "}
@@ -36,6 +83,17 @@ const Slide = React.forwardRef((props: any, ref: any) => {
           End Lesson
         </Button>
       </div>
+    );
+  }
+
+  if (props.isEnd) {
+    return (
+      <SlideIntro className="text-center"ref={ref}>
+        {props.children}
+        {props.displayButton ? button : null}
+        <br />
+        <br />
+      </SlideIntro>
     );
   }
 
@@ -66,24 +124,22 @@ Slide.defaultProps = {
   isStart: false,
   isEnd: false,
   displayButton: true,
-  isCurrent:false
+  isCurrent: false,
 };
 
 const SlideScreenBlock = styled.div`
   width: 100%;
   padding-top: 20px;
   height: calc(100vh - 80px);
-
 `;
 
 const SlideIntro = styled.div`
-  display:flex;
+  display: flex;
   flex-direction: column;
   justify-content: center;
   width: 100%;
   padding-top: 20px;
   height: calc(100vh - 80px);
-
 `;
 
 const SlideBlock = styled.div`
